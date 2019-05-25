0dd6bc59ed62973f95c175f9a634ef297c875909
sneric <sneric.smith@gmail.com>
2019-05-25 15:37:49 -0500

    initial commit

---
+++ ViewFinder2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewFinder2
+//
+//  Created by Eric Smith on 5/25/19.
+//  Copyright © 2019 Eric Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
